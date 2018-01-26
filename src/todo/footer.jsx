083c882e91774a67ee69d86d32ae837df070922b
wangruifang <wangruifang@buqiuren.com.cn>
2018-01-26 16:39:45 +0800

    first commit

---
+++ src/todo/footer.jsx
@@ -0,0 +1,15 @@
+import '../assects/style/footer.styl'
+export default {
+	data() {
+		return {
+			author:'Jokcy'
+		}
+	},
+	render() {
+		return(
+			<div id="footer">
+				<span>Written by {this.author}</span>
+			</div>
+		)
+	}
+}
---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+const path=require('path');
+
+const HTMLPlugin=require('html-webpack-plugin');
+
+const webpack = require('webpack');
+
+const isDev = process.env.NODE_ENV === "developmemt"
+
+const config ={
+	target:'web',
+	entry:path.join(__dirname,'src/index.js'),
+	output:{
+		filename:'bundle.js',
+		path:path.join(__dirname,'dist')
+	},
+	module:{
+		rules:[ 
+			{
+				test:/\.vue$/,
+				loader:'vue-loader'
+			},
+			{
+				test:/\.jsx$/,
+				loader:'babel-loader'
+			},
+			{
+				test:/\.css$/,
+				use:[
+						'style-loader',
+						'css-loader'
+					]
+			},
+			{
+				test:/\.styl/,
+				use:[
+						'style-loader',
+						'css-loader',
+						{
+							loader: 'postcss-loader',
+							options: {
+								sourceMap :true,
+							}
+						},
+						'stylus-loader'
+					]
+			},
+			{
+				test:/\.(jpg|png|jpeg|svg|gif)$/,
+				use:[
+						{
+							loader:'url-loader',
+							options:{
+								limit : 1024,
+								name :'[name]-aaa.[ext]'
+							}
+						}
+					
+				]
+			}
+		]
+	},
+	plugins :[
+		new webpack.DefinePlugin({
+			'procee.env':{
+				NODE_ENV : isDev ? '"developmemt"':'"production"'
+			}
+		}),
+		new HTMLPlugin()
+	]
+}
+
+if(isDev){
+	config.devtool = '#cheap-module-eval-source-map'
+	config.devServer={
+		port:'8000',
+		host:'0.0.0.0',
+		overlay:{
+			errors:true
+		},
+		// open:true //自动打开浏览器
+		hot:true//热加载
+	}
+	config.plugins.push(
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoEmitOnErrorsPlugin()
+	)
+}
+
+module.exports = config 
